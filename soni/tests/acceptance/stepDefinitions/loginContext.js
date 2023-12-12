7deef055a7ece3d8d8f3198e3cca580553962d9f
SoniGautam <gautam.sony575@gmail.com>
2023-12-12 15:22:39 +0545

    Login Test using OOP

--- soni/tests/acceptance/stepDefinitions/loginContext.js
+++ soni/tests/acceptance/stepDefinitions/loginContext.js
@@ -1,25 +1,34 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
 const { expect } = require("@playwright/test")
-
-//define selector
+const assert = require("assert")
+const LoginPage = require("../PageObjects/LoginPage.js")
+const loginPage = new LoginPage;
 
 Given('the user has browsed to the login page', async function () {
-  await page.goto(url);
-  await expect(page).toHaveURL(url)
+  await loginPage.gotoLoginPage();
+  await expect(page).toHaveURL(loginPage.loginUrl)
 });
 
+Given('user has logged in with username {string} and password {string}', async function (username, password) {
+  await loginPage.logintoHomePage(username, password)
+});
 
 When('user logins with username {string} and password {string}', async function (username, password) {
-  await page.fill(usernameSelector,username)
-  await page.fill(passwordSelector,password)
-  await page.click(loginButtonSelector)
+  await loginPage.logintoHomePage(username, password)
+});
+
+Then('user should be directed to the homepage', async function () {
+  await expect(page).toHaveURL(loginPage.homePageUrl)
+});
+
+Then('user should see {string} message', async function (expectedMessage) {
+  const actualMessage = await page.locator(loginPage.messageSelector).textContent()
+  assert.equal(
+    expectedMessage,
+    actualMessage,
+    `Expected message is ${expectedMessage} and actual message is ${actualMessage}.`
+    )
 });
 
 
-Then('user should redirect to the homepage', async function () {
- 
- await expect(page).toHaveURL(homepageUrl)
 
-});
-
-  
