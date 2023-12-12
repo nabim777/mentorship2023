6bdecb8008a65bbdc2ab35818694a3c000f3f6b1
Aakrity17 <aakritycpg@gmail.com>
2023-12-12 16:19:58 +0545

    Added test for login features

--- akriti/tests/acceptance/PageObject/LoginPage.js
+++ akriti/tests/acceptance/PageObject/LoginPage.js
@@ -1,14 +1,27 @@
+// const puppeteer =require('puppeteer')
+
 class LoginPage {
   constructor() {
-    //define selector
+    //Url
+    this.url = "http://localhost:8080/login";
+    this.homePageUrl = "http://localhost:8080/files/";
+
+    //Define selector
+    this.usernameSelector = '//input[@placeholder="Username"]'; 
     this.passwordSelector = '//input[@placeholder="Password"]';
     this.loginButtonSelector = '//input[@type="submit"]';
-this.usernameselector = '//input[@type = "username"]';
 
-    // WE paste it from the file "loginContent.js " so that we don't need to repeat it again
-    //
-
-    const url = "http://localhost:8080/login";
-    const url1 = "http://localhost:8080/files";
   }
+
+  async gotoLoginPage() {
+    await page.goto(this.url);
+  }
+
+  async loginHomePage(username, password) {
+    await page.fill(this.usernameSelector, username);
+    await page.fill(this.passwordSelector, password);
+    await page.click(this.loginButtonSelector);
+  }
+
 }
+module.exports = LoginPage;
