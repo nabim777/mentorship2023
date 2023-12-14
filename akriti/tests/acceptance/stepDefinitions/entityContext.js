97072e2ff99690d7a9bce7460d90ff111e4dcc70
Aakrity17 <aakritycpg@gmail.com>
2023-12-14 13:43:09 +0545

    Login feature sucessfully testedggg

---
+++ akriti/tests/acceptance/stepDefinitions/entityContext.js
@@ -0,0 +1,25 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect, test, _android } = require('@playwright/test');
+const assert = require("assert")
+
+
+
+
+
+
+Given('user logged in with username {string} and password {string}', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+When('user creates a file {string} with content {string}', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  Then('user should see the {string} file', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
