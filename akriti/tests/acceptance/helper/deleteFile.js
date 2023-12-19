9cc6a9f1af890286bb7a5cc68c1f1bc7b74c4099
Aakrity17 <aakritycpg@gmail.com>
2023-12-19 16:00:30 +0545

    deleteFile added

---
+++ akriti/tests/acceptance/helper/deleteFile.js
@@ -0,0 +1,46 @@
+const axios = require('axios')
+
+async function getXauthToken() {
+    try {
+
+        const res = await axios.post('http://localhost:8080/api/login', {
+            username: 'admin',
+            password: 'admin'
+        })
+        return res.data
+    } catch (error) {
+        console.error("Error:", error);
+    }
+
+}
+
+const filesToDelete = []
+
+async function deleteFile(filename) {
+    try {
+        const res = await axios.delete(`http://localhost:8080/api/resources/${filename}`, {
+            headers: {
+                "X-Auth": await getXauthToken()
+            }
+        })
+        return res.data
+    } catch (error) {
+        console.error("Error:", error);
+    }
+
+}
+
+async function cleanUpTempFiles() {
+    for (let i = 0; i < filesToDelete.length; i++) {
+        await deleteFile(filesToDelete[i])
+    }
+}
+function swapFileOnRename(oldfileName, newfileName) {
+    const fileToSwapIndex = filesToDelete.findIndex((file) => file == oldfileName);
+    filesToDelete[fileToSwapIndex] = newfileName;
+}
+
+exports.deleteFile = deleteFile
+exports.cleanUpTempFiles = cleanUpTempFiles
+exports.filesToDelete = filesToDelete
+exports.swapFileOnRename = swapFileOnRename
