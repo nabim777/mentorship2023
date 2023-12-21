14c56d3693410c0e76aef238f57877be5d785e6c
SoniGautam <gautam.sony575@gmail.com>
2023-12-21 16:49:15 +0545

    Added Create user feature

---
+++ soni/tests/acceptance/stepDefinitions/createUserContext.js
@@ -0,0 +1,12 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+When('admin browsed to the setting', function () {
+    
+  });
+
+
+  When('creates user with following details', function (dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    console.log(dataTable)
+  });
+
