ed735ce97a66d6265d6d9cd70798bfc1d8b6c8fa
Aakrity17 <aakritycpg@gmail.com>
2023-12-22 14:53:08 +0545

    addressing the review

--- akriti/tests/acceptance/PageObjects/LoginPage.js
+++ akriti/tests/acceptance/PageObjects/LoginPage.js
@@ -1,24 +1,24 @@
 class LoginPage {
   constructor() {
-      //defining Selectors
-      this.usernameSelector = "//input[@placeholder='Username']"
-      this.loginButtonSelector = "//input[@type='submit']"
-      this.passwordSelector = "//input[@placeholder='Password']"
-      this.messageSelector = ".wrong"        
-
-      //launch url
-      this.serverUrl = 'http://localhost:8080/';
-      this.loginUrl = this.serverUrl+"login";
-       this.homeUrl = this.serverUrl+"files/"
+    //defining Selectors
+    this.usernameSelector = "//input[@placeholder='Username']";
+    this.loginButtonSelector = "//input[@type='submit']";
+    this.passwordSelector = "//input[@placeholder='Password']";
+    this.messageSelector = ".wrong";
+    this.serverUrl = "http://localhost:8080/";
+    this.loginUrl = this.serverUrl + "login";
+    this.homeUrl = this.serverUrl + "files/";
   }
 
   async gotoLoginPage() {
-      await page.goto(this.loginUrl);
+    await page.goto(this.loginUrl);
   }
 
-   async logintoHomePage(username, password) {
-      await page.fill(this.usernameSelector, username);
-      await page.fill(this.passwordSelector, password);
-      await page.click(this.loginButtonSelector);
-   }}
-module.exports = LoginPage
+  async logintoHomePage(username, password) {
+    await page.fill(this.usernameSelector, username);
+    await page.fill(this.passwordSelector, password);
+    await page.click(this.loginButtonSelector);
+  }
+}
+
+module.exports = LoginPage;
--- akriti/tests/acceptance/stepDefinitions/createFileContext.js
+++ akriti/tests/acceptance/stepDefinitions/createFileContext.js
@@ -1,14 +1,13 @@
-const { Given, When, Then } = require("@cucumber/cucumber");
+const { When, Then } = require("@cucumber/cucumber");
 const { expect } = require("@playwright/test");
 const util = require('util');
 const HomePage = require("../PageObjects/HomePage.js");
+
 const homePage = new HomePage();
 
-When(
-  "the user creates a new file {string} with content {string}",
-  async function (fileName, content) {
+When("the user creates a new file {string} with content {string}",async function (fileName, content) {
     await homePage.createFile(fileName, content);
-  });
+});
 
 Then("the user should be able to see {string} file", async function (fileName) {
     const fileLocator = page.locator(util.format(homePage.fileNameSelector, fileName));
