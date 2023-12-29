9c30f45b11699236a2f27be45a32c2b9f8d5a083
SoniGautam <gautam.sony575@gmail.com>
2023-12-29 17:56:29 +0545

    resolve the file name

--- soni/tests/acceptance/stepDefinitions/createFileContext.js
+++ soni/tests/acceptance/stepDefinitions/createFileContext.js
@@ -1,15 +1,21 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
 const { expect } = require("@playwright/test")
 const util = require('util');
+const assert = require ('assert');
 const HomePage = require("../PageObjects/HomePage.js")
 const homePage = new HomePage
 
 When('admin creates a file {string} with content {string}', async function (fileName, fileContent) {
-  await homePage.createFile(fileName,fileContent)
+  await homePage.createFile(fileName, fileContent)
 });
-  
+
 Then('admin should see the {string} file', async function (fileName) {
-    const filelist = util.format(homePage.fileSelector, fileName);
-    expect(await page.locator(filelist)).toBeVisible() 
-  });
-  
+  const actualFile = await homePage.getFileFolderName(fileName);
+  const expectedFile = fileName;
+  assert.equal(
+    actualFile,
+    expectedFile,
+    `Expected File to be "${expectedFile}" but receive File "${actualFile}"`
+  )
+});
+
